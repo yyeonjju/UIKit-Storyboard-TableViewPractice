53e185e63e142486d942ffecd24165e26241855a
yyeonjju <llilyha123@gmail.com>
2023-10-25 21:32:52 +0900

    스토리보드로 테이블뷰 만들기

---
+++ TableViewPractice/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  TableViewPractice
+//
+//  Created by 하연주 on 2023/10/25.
+//
+
+import UIKit
+
+struct Movie {
+    var movieImage : UIImage?
+    let movieName : String
+    let movieDescription : String
+}
---
+++ TableViewPractice/MovieCell.swift
@@ -0,0 +1,31 @@
+//
+//  MovieCell.swift
+//  TableViewPractice
+//
+//  Created by 하연주 on 2023/10/25.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var mainImageView: UIImageView!
+    
+    @IBOutlet weak var movieNameLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
